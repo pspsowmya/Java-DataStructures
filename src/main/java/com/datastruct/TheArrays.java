5e3f428d4729296c295c64ffd694b652e38729c9
panthangiswapna <pspsowmya@gmail.com>
2022-03-16 17:04:15 +0000

    initial commit

---
+++ src/main/java/com/datastruct/TheArrays.java
@@ -0,0 +1,46 @@
+package com.datastruct;
+
+import java.util.Arrays;
+
+public class TheArrays {
+
+    public static void main(String[] args) {
+        String[] colors = new String[5];
+        colors[0] = "purple";
+        colors[1] = "blue";
+
+        System.out.println(Arrays.toString(colors));
+
+        System.out.println(colors[0]);
+        System.out.println(colors[1]);
+        System.out.println(colors[2]);
+        System.out.println(colors[3]);
+        System.out.println(colors[4]);
+        //System.out.println(colors[5]);
+
+        colors[2]= "yellow";
+
+        System.out.println(Arrays.toString(colors));
+
+        int[] numbers = {100,200};
+
+        //looping through arrays
+        //Using for loop
+        for(int i=0;i< colors.length;i++){
+            System.out.println(colors[i]);
+        }
+        //Reverse looping
+        for(int i= colors.length-1;i>=0;i--){
+            System.out.println(colors[i]);
+        }
+        System.out.println(colors.length);
+
+        //Using enhanced for loop
+        for(String color:colors){
+            System.out.println(color);
+        }
+
+        //Using foreach of Java 8
+        Arrays.stream(colors).forEach(System.out::println);
+    }
+}
